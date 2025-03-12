a5a2bc12b9e1900bb6df5c11204adda3eb748200
RahwaZeslusHaile <rahwazeslase@gmail.com>
2025-03-12 16:27:57 +0000

    task 7 done

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,4 +1,4 @@
- const {selectArticleById,fetchArticles} = require('../models/articles.model')
+ const {selectArticleById,fetchArticles,modifyArticleVotes } = require('../models/articles.model')
  
  exports.getArticlesById=(request,response,next)=>{
   const{article_id} =request.params;
@@ -14,3 +14,16 @@
         response.status(200).send({article})
     }).catch(next)
      }
+
+     
+     
+     exports.updateArticleVotes = (request, response, next) => {
+        const { article_id } = request.params;
+        const { inc_votes } = request.body;
+    
+        modifyArticleVotes(article_id, inc_votes)
+            .then((updatedArticle) => {
+                response.status(200).send({ article: updatedArticle });
+            })
+            .catch(next);
+    };
--- models/articles.model.js
+++ models/articles.model.js
@@ -31,4 +31,21 @@ exports.fetchArticles = () => {
            return articles;
 })
 }
-  
+exports.modifyArticleVotes = (article_id, inc_votes) => {
+    if (isNaN(inc_votes)) {
+        return Promise.reject({ status: 400, msg: 'Invalid vote value' });
+    }
+
+    return db.query(
+        `UPDATE articles 
+        SET votes = votes + $1 
+        WHERE article_id = $2
+        RETURNING *;`,[inc_votes, article_id]
+    ).then(({ rows }) => {
+       
+        if (rows.length === 0) {
+            return Promise.reject({ status: 404, msg: 'Article not found' });
+        }
+        return rows[0]; 
+    });
+};
